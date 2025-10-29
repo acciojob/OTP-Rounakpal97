24d5280e63095e587001d7a4c9c8e2f3a7243b7a
Rounakpal97 <rounakpal97@gmail.com>
2025-10-29 11:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+  code.addEventListener('input', (e) => {
+    const value = e.target.value;
+    if (value && idx < codes.length - 1) {
+      codes[idx + 1].focus();
+    }
+  });
+
+  code.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      code.value = '';
+      if (idx > 0) {
+        codes[idx - 1].focus();
+      }
+    }
+  });
+});
